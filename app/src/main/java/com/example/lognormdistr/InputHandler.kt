283c0fc99b7a5a8cc5391e97c31f1dea91b02ee2
Max Yustus <ustusmaksim23@gmail.com>
2024-05-15 22:26:08 +0300

    Minor changes.

--- app/src/main/java/com/example/lognormdistr/InputHandler.kt
+++ app/src/main/java/com/example/lognormdistr/InputHandler.kt
@@ -5,7 +5,6 @@ object InputHandler {
     const val nonNumericInputMsg = "Одно из значений неверно"
 
     fun handle(str1: String, str2: String, onSuccess: (String, String) -> Any, onError: (String) -> Any): Any {
-        println("str1: \"$str1\"\nstr2: \"$str2\"")
         if (str1.isEmpty() || str2.isEmpty()) {
             return onError(emptyInputMsg)
         }
